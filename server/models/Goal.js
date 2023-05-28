41a1f2a5c1f5071a4fc424a73c363b087f07cd7f
Chris Daniels <danielschris96@gmail.com>
2023-05-28 16:23:39 -0700

    added mongodb packages and started model files

--- server/models/Goal.js
+++ server/models/Goal.js
@@ -1,1 +1,12 @@
 const mongoose = require('mongoose');
+
+const goalSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  AmountToSave: { type: Number, required: true },
+  byDate: { type: Date, required: true },
+  shortTerm: { type: Boolean, default: false }
+});
+
+const Goal = mongoose.model('Goal', goalSchema);
+
+module.exports = Goal;
--- server/models/Setting.js
+++ server/models/Setting.js
@@ -1,1 +1,11 @@
 const mongoose = require('mongoose');
+
+const settingSchema = new mongoose.Schema({
+  light: { type: Boolean, default: false },
+  currency: { type: String },
+  language: { type: String }
+});
+
+const Setting = mongoose.model('Setting', settingSchema);
+
+module.exports = Setting;
