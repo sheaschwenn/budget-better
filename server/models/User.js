7e0edfca3d2eeb85a250adcb7a15ad0516a7e410
Chris Daniels <danielschris96@gmail.com>
2023-05-30 13:26:10 -0700

    added validators to models

--- server/models/User.js
+++ server/models/User.js
@@ -7,11 +7,16 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    required: true
+    required: true,
+    trim: true,
+    unique: true, // Ensure email is unique
+    lowercase: true, // Convert email to lowercase
+    match: /^\S+@\S+\.\S+$/ // Validate email format using regex
   },
   password: {
     type: String,
-    required: true
+    required: true,
+    minlength: 6 // Minimum password length of 6 characters
   },
   expenses: [{
     type: mongoose.Schema.Types.ObjectId,
