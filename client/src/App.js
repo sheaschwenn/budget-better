27d003c63f53ad1667a1fe61d5674f687da09f0c
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-05-29 23:47:08 -0500

    added chatbot to the app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Chatbot from "./chatbot";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //applying the ApolloProvider component to the App component, we can then use the useQuery() Hook in any component to make a GraphQL query.
+    <ApolloProvider client={client}>
+      {/* //router component to handle the navigation between pages */}
+      <Router>
+        <div>
+          {/* <PageContainer> */}
+          <NavigateTabs />
+          <Routes>
+            <Route exact path="/" element={<Chatbot />} />
+          </Routes>
+          {/* </PageContainer> */}
+        </div>
+      </Router>
+      <Footer />
+    </ApolloProvider>
   );
 }
 
