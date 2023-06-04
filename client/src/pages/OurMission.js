494000abd568994208806bb826aaecdc8da10c75
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-06-04 14:37:19 -0500

    completed the OurMission.js

--- client/src/pages/OurMission.js
+++ client/src/pages/OurMission.js
@@ -1,14 +1,8 @@
-const links = [
-  { name: "Open roles", href: "#" },
-  { name: "Internship program", href: "#" },
-  { name: "Our values", href: "#" },
-  { name: "Meet our leadership", href: "#" },
-];
 const stats = [
-  { name: "Offices worldwide", value: "12" },
-  { name: "Full-time colleagues", value: "300+" },
-  { name: "Hours per week", value: "40" },
-  { name: "Paid time off", value: "Unlimited" },
+  { name: "Individuals gained financial Freedom", value: "10,000+" },
+  { name: "Five Star Reviews", value: "300+" },
+  { name: "At Your Service", value: "24/7" },
+  { name: "Peace of Mind", value: "Unlimited" },
 ];
 
 export default function Example() {
@@ -46,22 +40,19 @@ export default function Example() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-            Work with us
+            Our Mission
           </h2>
           <p className="mt-6 text-lg leading-8 text-gray-300">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-            fugiat aliqua.
+            Our purpose at Budget Better is to empower individuals to master
+            their financial journey through our intuitive financial management
+            app. We aim to simplify financial planning, offering user-friendly
+            tools for managing income, expenses, and investments. Our AI chatbot
+            aids users round the clock, fostering financial literacy and
+            independence. We envision a future where everyone can understand and
+            enhance their financial health with confidence.
           </p>
         </div>
         <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-          <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
-            {links.map((link) => (
-              <a key={link.name} href={link.href}>
-                {link.name} <span aria-hidden="true">&rarr;</span>
-              </a>
-            ))}
-          </div>
           <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
             {stats.map((stat) => (
               <div key={stat.name} className="flex flex-col-reverse">
