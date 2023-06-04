efb4c05d6c4b7e7ded56ecbe73b344067b1e7349
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-06-04 14:08:59 -0500

    added a heropage.js rought draft , this is the page that appears when the person first enters the site

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useQuery } from "@apollo/client";
 
-import { GET_ME } from '../utils/queries';
+import { GET_ME } from "../utils/queries";
 
 const Home = () => {
   const { loading, data } = useQuery(GET_ME);
@@ -18,14 +18,21 @@ const Home = () => {
             <>
               <h2 className="text-center">Welcome to BudgetBetter</h2>
               <div className="flex-row justify-center">
-                <div className="col-12 col-md-10 my-3">
-                </div>
+                <div className="col-12 col-md-10 my-3"></div>
               </div>
               <div className="flex-row justify-center mt-3">
-                <Link to="/login" className="btn btn-primary mr-2">Login</Link>
-                <Link to="/signup" className="btn btn-primary">Signup</Link>
-                <Link to="/dashboard" className="btn btn-primary">Dashboard</Link>
-                <Link to="/cashbot" className="btn btn-primary">Cashbot</Link>
+                <Link to="/login" className="btn btn-primary mr-2">
+                  Login
+                </Link>
+                <Link to="/signup" className="btn btn-primary">
+                  Signup
+                </Link>
+                <Link to="/dashboard" className="btn btn-primary">
+                  Dashboard
+                </Link>
+                <Link to="/cashbot" className="btn btn-primary">
+                  Cashbot
+                </Link>
               </div>
             </>
           )}
