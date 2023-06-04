36d4f7945fdc32961437fbe23fec362b48995e24
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-06-04 14:21:14 -0500

    added ourmission rough draft/ template

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../utils/mutations';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
-import companyLogo from '../assets/images/BudgetBetterLogo.png';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
+import companyLogo from "../assets/images/BudgetBetterLogo.png";
 
-
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 
 const Login = (props) => {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ email: "", password: "" });
   const [login, { error, data }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
@@ -40,8 +39,8 @@ const Login = (props) => {
 
     // clear form values
     setFormState({
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     });
   };
 
@@ -98,15 +97,13 @@ const Login = (props) => {
 
           {/* Display error message */}
           {error && (
-            <div className="my-3 p-3 bg-danger text-white">
-              {error.message}
-            </div>
+            <div className="my-3 p-3 bg-danger text-white">{error.message}</div>
           )}
 
           {/* Display success message */}
           {data && (
             <div className="my-3 p-3 bg-success text-white">
-              Success! You may now head{' '}
+              Success! You may now head{" "}
               <Link to="/">back to the homepage.</Link>
             </div>
           )}
@@ -117,3 +114,103 @@ const Login = (props) => {
 };
 
 export default Login;
+
+// /*
+//   This example requires some changes to your config:
+
+//   ```
+//   // tailwind.config.js
+//   module.exports = {
+//     // ...
+//     plugins: [
+//       // ...
+//       require('@tailwindcss/forms'),
+//     ],
+//   }
+//   ```
+// */
+// export default function Example() {
+//   return (
+//     <>
+//       {/*
+//         This example requires updating your template:
+
+//         ```
+//         <html class="h-full bg-white">
+//         <body class="h-full">
+//         ```
+//       */}
+//       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+//         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+//           <img
+//             className="mx-auto h-10 w-auto"
+//             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+//             alt="Your Company"
+//           />
+//           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+//             Sign in to your account
+//           </h2>
+//         </div>
+
+//         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+//           <form className="space-y-6" action="#" method="POST">
+//             <div>
+//               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+//                 Email address
+//               </label>
+//               <div className="mt-2">
+//                 <input
+//                   id="email"
+//                   name="email"
+//                   type="email"
+//                   autoComplete="email"
+//                   required
+//                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+//                 />
+//               </div>
+//             </div>
+
+//             <div>
+//               <div className="flex items-center justify-between">
+//                 <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+//                   Password
+//                 </label>
+//                 <div className="text-sm">
+//                   <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+//                     Forgot password?
+//                   </a>
+//                 </div>
+//               </div>
+//               <div className="mt-2">
+//                 <input
+//                   id="password"
+//                   name="password"
+//                   type="password"
+//                   autoComplete="current-password"
+//                   required
+//                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+//                 />
+//               </div>
+//             </div>
+
+//             <div>
+//               <button
+//                 type="submit"
+//                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+//               >
+//                 Sign in
+//               </button>
+//             </div>
+//           </form>
+
+//           <p className="mt-10 text-center text-sm text-gray-500">
+//             Not a member?{' '}
+//             <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+//               Start a 14 day free trial
+//             </a>
+//           </p>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
