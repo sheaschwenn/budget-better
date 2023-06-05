936fa73483347aa8339b029d73e439609605311e
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-06-05 10:54:07 -0500

    incorporated the navbar and footer in the about.js, contact.js, demo.js and features.js

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -39,29 +39,17 @@ export default function Contact() {
       console.error("Form submission failed");
     }
   };
-
   return (
-    <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div
-        className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
-        aria-hidden="true"
-      >
-        <div
-          className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        />
-      </div>
-      <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          Contact sales
-        </h2>
-        <p className="mt-2 text-lg leading-8 text-gray-600">
-          Aute magna irure deserunt veniam aliqua magna enim voluptate.
-        </p>
-      </div>
+    <div className="flex flex-col min-h-screen bg-gray-50">
+      <Navbar />
+      <main className="flex-grow">
+        <div className="py-16 px-4 sm:px-6 lg:px-8 mx-auto max-w-7xl">
+          <div className="text-center">
+            <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Contact Us</h2>
+            <p className="mt-2 text-3xl font-extrabold text-gray-900 sm:text-4xl">How can we assist you?</p>
+          </div>
+          <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+            <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
       <form
         action="#"
         method="POST"
@@ -180,6 +168,11 @@ export default function Contact() {
           </div>
         </div>
       </form>
+      </div>
+          </div>
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
