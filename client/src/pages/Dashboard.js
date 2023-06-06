c77f923ca7e991bc65c76317f348f7bcbc96b588
GitDidrik <lindberg.didrik@gmail.com>
2023-06-06 14:11:55 -0700

    darkmode pages update

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef, useContext } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { generateExpensePieChart } from '../utils/chart';
-import { ThemeContext } from '../utils/ThemeContext';
+
 import { GET_EXPENSES } from '../utils/queries';
 import { useQuery } from '@apollo/client';
 
@@ -8,7 +8,7 @@ import { useQuery } from '@apollo/client';
 const Dashboard = () => {
   const chartRef = useRef(null);
 
-  const { isDarkMode } = useContext(ThemeContext);
+
 
 
   const { loading, error, data } = useQuery(GET_EXPENSES);
@@ -19,16 +19,12 @@ const Dashboard = () => {
     }
   }, [data]);
 
-  const styles = {
-    backgroundColor: isDarkMode ? '#000000' : '#ffffff',
-    color: isDarkMode ? '#ffffff' : '#000000',
-  };
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
   return (
-    <div style={styles}>
+    <div>
       <section className="dashboard-section">
         <h2>Welcome to Your Dashboard</h2>
         <div className="finance-info">
