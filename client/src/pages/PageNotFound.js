a2085ab1bd770ff1077cca73c36328faa8c15173
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-06-09 16:44:23 -0500

    changed <a> to <Link>

--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../utils/ThemeContext";
+import { Link } from "react-router-dom";
 export default function PageNotFound() {
   const { isDarkMode } = useContext(ThemeContext);
   const styles = {
@@ -20,18 +21,18 @@ export default function PageNotFound() {
               Sorry, we couldn’t find the page you’re looking for.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="/"
+              <Link
+                to="/"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Go back home
-              </a>
-              <a
-                href="/contact"
+              </Link>
+              <Link
+                to="/contact"
                 className="text-sm font-semibold text-gray-900"
               >
                 Contact support <span aria-hidden="true">&rarr;</span>
-              </a>
+              </Link>
             </div>
           </div>
         </main>
