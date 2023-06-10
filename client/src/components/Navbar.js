be57e3c9b8d692075d093c1ddb1b0260ada2441d
unknown <danielschris96@gmail.com>
2023-06-09 22:31:51 -0700

    added logout button and fixed login button route

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState, useContext } from "react";
 import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
 import { ThemeContext } from "../utils/ThemeContext";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { FaSun, FaMoon } from "react-icons/fa";
 import logo from "./BudgetBetterLogo.png";
 import {
@@ -15,6 +15,7 @@ import {
 } from "@heroicons/react/24/outline";
 import { ChevronDownIcon, PlayCircleIcon } from "@heroicons/react/20/solid";
 import { Link } from "react-router-dom";
+import AuthService from "../utils/auth";
 
 const products = [
   {
@@ -60,6 +61,14 @@ const callsToAction = [
 
 export default function Navbar() {
   // const isLoggedIn = AuthService.loggedIn();
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    AuthService.logout();
+    navigate('/login');
+  };
+
   const location = useLocation();
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const { isDarkMode, toggleDarkMode } = useContext(ThemeContext);
@@ -206,6 +215,12 @@ export default function Navbar() {
           >
             Log in <span aria-hidden="true">&rarr;</span>
           </Link>
+          <button
+                className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                onClick={handleLogout}
+              >
+                Log out
+              </button>
         </div>
       </nav>
       <Dialog
@@ -290,6 +305,12 @@ export default function Navbar() {
               >
                 Log in
               </Link>
+              <button
+                className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                onClick={handleLogout}
+              >
+                Log out
+              </button>
             </div>
           </div>
         </Dialog.Panel>
