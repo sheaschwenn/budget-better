aba1cb235d93af6d0e074b75c5a2947c609c82d3
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-06-11 18:00:26 -0500

    features page is done

--- client/src/pages/Features.js
+++ client/src/pages/Features.js
@@ -66,15 +66,15 @@ export default function Example() {
               </p>
               <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                 {features.map((feature) => (
-                  <div key={feature.name} className="relative pl-9">
-                    <dt className="inline font-semibold text-gray-900">
+                  <div key={feature.name} className="pl-9">
+                    <dt className="flex items-center font-semibold text-gray-900">
                       <feature.icon
-                        className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+                        className="h-5 w-5 text-indigo-600 mr-2"
                         aria-hidden="true"
                       />
                       {feature.name}
-                    </dt>{" "}
-                    <dd className="inline">{feature.description}</dd>
+                    </dt>
+                    <dd>{feature.description}</dd>
                   </div>
                 ))}
               </dl>
